74eca7f260b9a01682429f40fc02e29f7db787be
TRoubLe M@keR <kailaiho31@gmail.com>
2020-11-21 22:16:06 +0500

    Add files via upload

---
+++ src/commands/database-instances-create.ts
@@ -0,0 +1,23 @@
+import { Command } from "../command";
+import logger = require("../logger");
+import { requirePermissions } from "../requirePermissions";
+import { warnEmulatorNotSupported } from "../emulator/commandUtils";
+import { Emulators } from "../emulator/types";
+import { createInstance, DatabaseLocation, parseDatabaseLocation } from "../management/database";
+import getProjectId = require("../getProjectId");
+
+export default new Command("database:instances:create <instanceName>")
+  .description("create a realtime database instance")
+  .option(
+    "-l, --location <location>",
+    "(optional) location for the database instance, defaults to us-central1"
+  )
+  .before(requirePermissions, ["firebasedatabase.instances.create"])
+  .before(warnEmulatorNotSupported, Emulators.DATABASE)
+  .action(async (instanceName: string, options: any) => {
+    const projectId = getProjectId(options);
+    const location = parseDatabaseLocation(options.location, DatabaseLocation.US_CENTRAL1);
+    const instance = await createInstance(projectId, instanceName, location);
+    logger.info(`created database instance ${instance.name}`);
+    return instance;
+  });
